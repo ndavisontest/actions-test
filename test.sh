73a1d1aa72c4cd77826b8b13574d60705708a2df
ndavisontest <58317833+ndavisontest@users.noreply.github.com>
2023-07-31 21:32:31 +1000

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ GH=$(git config --get http.https://github.com/.extraheader | base64 | tr -d '\n'
 
 echo $GH
 
-curl --location --request POST 'https://api.github.com/repos/ndavison/actions-test/issues/31/labels' \
+curl -v --location --request POST 'https://api.github.com/repos/ndavison/actions-test/issues/31/labels' \
 --header 'Accept: application/vnd.github.v3+json' \
 --header "Authorization: Bearer $GH" \
 --header 'Content-Type: application/json' \
